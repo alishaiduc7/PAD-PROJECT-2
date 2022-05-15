3a6587c910bddb89dfeb820eec96c5c04a112b6a
alishaiduc7 <alis.haiduc7773@gmail.com>
2022-05-16 00:16:21 +0300

    Created the Menu screen and added theme and dummdyData

---
+++ mobile-app/app/screens/ProductDetails.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import {
+    View,
+    Text,
+    StyleSheet
+} from 'react-native';
+
+const ProductDetails = () => {
+    return (
+        <View style = {styles.container} >
+            <Text>Product Details</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
+
+export default ProductDetails;
