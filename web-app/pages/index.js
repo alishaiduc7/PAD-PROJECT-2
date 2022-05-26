363d0866f648b51f01283a216b30e1245aa2622c
alishaiduc7 <alis.haiduc7773@gmail.com>
2022-05-26 21:51:15 +0300

    Initial commit on the web app for the coffee shop

---
+++ web-app/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Soft Coffee online shop</title>
+        <meta name="description" content="Best coffee in town! Try it out" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+    </div>
+  )
+}
