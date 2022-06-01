8a243a146ea574a6f0207fd62fca4b7d9ac91dfc
Mihai Andreea <andreea.mihai@student.upt.ro>
2022-06-02 02:01:06 +0300

    Populated products DB

--- web-app/components/ProductCard.jsx
+++ web-app/components/ProductCard.jsx
@@ -5,10 +5,10 @@ import Image from "next/image"
 const ProductCard = ({ product }) => {
     return(
         <div className={styles.container}>
-           <Image src={product.image} alt="" width="500" height="500" className={styles.image}></Image> 
+           <Image src={product.image} alt="" width={250} height={250} objectFit="contain" className={styles.image}></Image> 
            <h1 className={styles.title}>{product.title}</h1>
            <span className={styles.price}>{product.prices[0]} lei</span>
-           <p className={styles.desc}>{product.desc}</p>
+           <p className={styles.desc}>{product.description}</p>
         </div>
     )
 }
