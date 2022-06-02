f8ba1f6679f294ccb2a7a15a36be88c43547ef6b
Mihai Andreea <andreea.mihai@student.upt.ro>
2022-06-02 03:28:51 +0300

    Created connection to product page

--- web-app/components/ProductCard.jsx
+++ web-app/components/ProductCard.jsx
@@ -1,15 +1,18 @@
 import styles from "../styles/ProductCard.module.css"
 import Image from "next/image"
+import Link from "next/link"
 
 
 const ProductCard = ({ product }) => {
     return(
+        <Link href={`/product/${product._id}`} passHref>
         <div className={styles.container}>
            <Image src={product.image} alt="" width={250} height={250} objectFit="contain" className={styles.image}></Image> 
            <h1 className={styles.title}>{product.title}</h1>
            <span className={styles.price}>{product.prices[0]} lei</span>
            <p className={styles.desc}>{product.description}</p>
         </div>
+        </Link>
     )
 }
 
